3d7caa4fbeb2f595a687e529dd969c8043f22bb8
soodraghav <raghav1sood@yahoo.co.in>
2021-01-28 19:13:23 -0600

    stack done

---
+++ 2_StacksAndQueues/1_StackExercises/7_minimum_bracket_reversals.py
@@ -0,0 +1,103 @@
+class LinkedListNode:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack:
+
+    def __init__(self):
+        self.num_elements = 0
+        self.head = None
+
+    def push(self, data):
+        new_node = LinkedListNode(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.num_elements += 1
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        temp = self.head.data
+        self.head = self.head.next
+        self.num_elements -= 1
+        return temp
+
+    def top(self):
+        if self.head is None:
+            return None
+        return self.head.data
+
+    def size(self):
+        return self.num_elements
+
+    def is_empty(self):
+        return self.num_elements == 0
+
+def minimum_bracket_reversals(input_string):
+    """
+    Calculate the number of reversals to fix the brackets
+
+    Args:
+       input_string(string): Strings to be used for bracket reversal calculation
+    Returns:
+       int: Number of bracket reversals needed
+    """
+    
+    if len(input_string)%2 != 0:
+        return None
+    
+    open = 0
+    inv = 0
+    
+    for brac in input_string:
+        
+        if brac == "{":
+            open = open + 1
+            
+        else:
+            
+            if open:
+                open -=1
+                
+            else:
+                open += 1 
+                inv += 1
+                
+    return inv + (open / 2)
+    
+def test_function(test_case):
+    input_string = test_case[0]
+    expected_output = test_case[1]
+    output = minimum_bracket_reversals(input_string)
+    
+    if output == expected_output:
+        print("Pass")
+    else:
+        print("Fail")
+
+test_case_1 = ["}}}}", 2]
+test_function(test_case_1)
+
+
+test_case_2 = ["}}{{", 2]          
+test_function(test_case_2)
+
+test_case_3 = ["{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{}}}}}", 13]
+
+test_function(test_case_1)
+
+test_case_4= ["}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{", 2]
+test_function(test_case_2)
+
+test_case_5 = ["}}{}{}{}{}{}{}{}{}{}{}{}{}{}{}", 1]
+
+test_function(test_case_3)
+
+
+            
+        
