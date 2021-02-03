ae4909c4f7dc7ce1d21c6d03826d4040090c8bfd
soodraghav <raghav1sood@yahoo.co.in>
2021-02-03 14:13:57 -0600

    Added Algo section

---
+++ Algorithms/1_Recursion/1_factorial.py
@@ -0,0 +1,17 @@
+
+def fact(n):
+    
+    if n == 0:
+        return 1
+        
+        
+    else:
+        
+        return n * fact(n-1)
+        
+        
+print(fact(5))
+
+
+
+            
